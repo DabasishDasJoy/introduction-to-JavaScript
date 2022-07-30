36b0a95538c23dd22af004e0dc0187c89e410119
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-07-30 21:56:43 +0600

    operations

---
+++ first.js
@@ -0,0 +1,12 @@
+var price = 200;
+var age = 26;
+var money = 700;
+var weight = 15;
+
+var name = "Dabasish Das Joy";
+var flag = true;
+var flag = true;
+
+var onionPrice = 10;
+var eggPrice = 15;
+console.log(onionPrice + eggPrice);
