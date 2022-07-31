8880b84688a1adf359c41ec89f763e67caa3bbc7
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-07-31 21:50:29 +0600

    array manipulations

---
+++ array.js
@@ -0,0 +1,18 @@
+var array = [25, 56, 57, 298, 2378, 98, 56];
+
+console.log(array.indexOf(298));
+
+array.push(89);
+console.log(array);
+// array.pop();
+// array.pop();
+array.unshift(78);
+var num = 2;
+num = num.toString();
+
+console.log(array);
+
+array.shift();
+console.log(array);
+
+
